58266965d4afd0bedbe3cf5cb220a9ffbb3109f9
Haritha314 <Hxv10720@ucmo.edu>
2021-06-22 13:57:28 -0500

    Update variables.tf

--- 12_keyvault_pwd/variables.tf
+++ 12_keyvault_pwd/variables.tf
@@ -13,13 +13,3 @@ variable "keyvault_name" {
   description = "Key Vault name in Azure"
 }
 
-variable "secret_name" {
-  type        = string
-  description = "Key Vault Secret name in Azure"
-}
-
-variable "secret_value" {
-  type        = string
-  description = "Key Vault Secret value in Azure"
-  sensitive   = true
-}
