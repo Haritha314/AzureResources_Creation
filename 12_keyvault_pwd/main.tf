63de609a3c60240dda8e529d3f7eff660f48e856
Haritha314 <Hxv10720@ucmo.edu>
2021-06-22 13:52:43 -0500

    Update main.tf

--- 12_keyvault_pwd/main.tf
+++ 12_keyvault_pwd/main.tf
@@ -45,9 +45,3 @@ resource "azurerm_key_vault" "keyvault" {
   
    }
 }
-
-resource "azurerm_key_vault_secret" "db-pwd" {
-  name         = var.secret_name
-  value        = var.secret_value
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
